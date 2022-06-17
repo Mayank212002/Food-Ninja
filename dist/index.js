f377fbc1d052b9f927b863d18488a6788b9c0299
DESKTOP-S3JB1RR\Mayank <mayankkm2100@gmail.com>
2022-06-17 16:44:37 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+const burger = document.querySelector('#burger');
+const menuy = document.querySelector('#menu');
+
+burger.addEventListener('click', () => {
+    
+    if(menu.classList.contains('hidden')){
+        menu.classList.remove('hidden');
+    }   
+    else{
+        menu.classList.add('hidden');
+    }
+
+})
+
+console.log("hhhhh");
